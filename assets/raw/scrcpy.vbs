0b57ce18210c0423f89f2d29af77504c3029667e
huangshengjie <daniel.huang.prc@gmail.com>
2021-04-20 10:19:07 +0800

    Add files via upload

---
+++ assets/raw/scrcpy.vbs
@@ -0,0 +1,2 @@
+Set ws = CreateObject("Wscript.Shell") 
+ws.run "cmd /c D:\scrcpy-win64-v1.16\shell.bat 192.168.218.56",vbhide 
---
+++ assets/raw/shell.bat
@@ -0,0 +1,7 @@
+set input=%1%
+echo Android Device ip: %input%
+D:\scrcpy-win64-v1.16\adb devices
+D:\scrcpy-win64-v1.16\adb disconnect 
+D:\scrcpy-win64-v1.16\adb connect %input%
+D:\scrcpy-win64-v1.16\adb devices
+D:\scrcpy-win64-v1.16\scrcpy
