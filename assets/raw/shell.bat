5a5dbd560f67db4e8708e068ccc1300213c6871c
huangshengjie <helloworkwork@outlook.com>
2021-06-03 21:53:36 +0800

    Delete shell.bat

--- assets/raw/shell.bat
+++
@@ -1,7 +0,0 @@
-set input=%1%
-echo Android Device ip: %input%
-D:\scrcpy-win64-v1.16\adb devices
-D:\scrcpy-win64-v1.16\adb disconnect 
-D:\scrcpy-win64-v1.16\adb connect %input%
-D:\scrcpy-win64-v1.16\adb devices
-D:\scrcpy-win64-v1.16\scrcpy -s %input%
